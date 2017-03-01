82585f490b2889f4a51cedfd80d2006e356880e1
HelixOS <vitaliy@thecto.co.il>
2017-03-01 11:10:44 +0200

    Update decision_graph.h

--- ecryptfs-utils/include/decision_graph.h
+++ ecryptfs-utils/include/decision_graph.h
@@ -23,9 +23,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
- *
- * Modifications Copyright (C) 2011-2017 TheCTO Technological Entrepreneurship & Consulting LTD
- *
  */
 
 #ifndef DECISION_GRAPH_H
