530251de0e846d27c5ecef39e7e03231eca8064a
HelixOS <vitaliy@thecto.co.il>
2017-03-01 11:05:22 +0200

    Update ecryptfs_key_mod_gpg.c

--- ecryptfs-utils/key_mod/ecryptfs_key_mod_gpg.c
+++ ecryptfs-utils/key_mod/ecryptfs_key_mod_gpg.c
@@ -16,6 +16,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
+ *
+ * Modifications Copyright (C) 2011-2017 TheCTO Technological Entrepreneurship & Consulting LTD
+ *
  */
 
 #include <fcntl.h>
